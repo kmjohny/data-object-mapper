6c6025399ce40296afbfc76ebd25c340d611c633
Johny k Mathew <kmjohny@users.noreply.github.com>
2016-12-16 08:50:22 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+## Object Mapper
+
+### Initialize project using spring cli
+spring init -j=1.8 -g=org.jkm.dataobjectmapper -a=data-object-mapper -n=data-object-mapper -d=cloud-starter -build=gradle -p=jar data-object-mapper
---
+++ src/main/java/com/example/DataObjectMapperApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataObjectMapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataObjectMapperApplication.class, args);
+	}
+}
